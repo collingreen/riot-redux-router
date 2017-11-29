d379a23ddfebda357f4cfce57894860585c49868
Collin Green <collin@collingreen.com>
2017-11-28 22:08:38 -0800

    add basic tests

---
+++ test/middleware.test.js
@@ -0,0 +1,42 @@
+var assert = require('chai').assert
+
+var redux = require('redux')
+var riot = require('riot')
+var rrr = require('../src/')
+
+var reducers = redux.combineReducers({
+  router: rrr.reducer
+})
+var middleware = redux.compose(redux.applyMiddleware(rrr.middleware))
+var store = redux.createStore(reducers, {}, middleware)
+
+describe('#store', function() {
+  it('should start with an empty string current_url', function() {
+    var state = store.getState()
+    assert.equal(state.router.current_url, '');
+  })
+
+  it('should store the current route in the state', function() {
+    var targetUrl = '/foo/bar/baz'
+    riot.route(targetUrl)
+    var state = store.getState()
+    assert.equal(
+      state.router.current_url,
+      targetUrl.substr(1)  // remove leading slash
+    )
+  })
+
+  it('should update the route as it changes', function() {
+    var firstUrl = '/foo'
+    var secondUrl = '/bar'
+
+    riot.route(firstUrl)
+    riot.route(secondUrl)
+
+    var state = store.getState()
+    assert.equal(
+      state.router.current_url,
+      secondUrl.substr(1)  // remove leading slash
+    )
+  })
+})
